77fa9397757bf9fb12e05197fda7ffb0a4d15070
IshtyMahin <ishty119@gmail.com>
2024-05-22 23:35:43 +0600

    owner card update

--- src/Components/OwnerCard/OwnerCard.jsx
+++ src/Components/OwnerCard/OwnerCard.jsx
@@ -4,7 +4,6 @@ import DetailCard from "../DetailCard/DetailCard";
 
 const OwnerCard = () => {
     const { user, allApartment } = useContext(AuthContext);
-
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
@@ -17,23 +16,19 @@ const OwnerCard = () => {
         return <span className="loading loading-spinner loading-lg"></span>;
     }
 
-    const ownerApartments = allApartment?.filter(apartment => apartment.owner_id === user.id);
-    console.log(ownerApartments);
-
-
-
+    const ownerApartments = allApartment?.filter(apartment => apartment?.owner_id === user?.id);
 
     return (
         <div>
             <div className='my-16'>
-                <h1 className='bg-slate-300 md:p-8 sm:p-6 border-slate-800 md:mx-16 text-2xl md:text-3xl lg:text-4xl my-6 text-center rounded-xl  font-bold'>
+                <h1 className='bg-slate-300 md:p-8 sm:p-6 border-slate-800 md:mx-16 text-2xl md:text-3xl lg:text-4xl my-6 text-center rounded-xl font-bold'>
                     Your Added Apartment
                 </h1>
-                {ownerApartments.length === 0 ? (
+                {ownerApartments?.length === 0 ? (
                     <p className="text-center text-gray-600">You have not added any apartments yet.</p>
                 ) : (
                     <div className='grid md:grid-cols-2 gap-6'>
-                        {ownerApartments.map(apartment => (
+                        {ownerApartments?.map(apartment => (
                             <DetailCard key={apartment.id} apartment={apartment} />
                         ))}
                     </div>
